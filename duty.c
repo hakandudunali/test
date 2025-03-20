729cd5f1f7c54afbd106362eafd0ee78f6f2efab
hakandudunali <hakandudunali@gmail.com>
2025-03-20 18:51:16 +0300

    update code

--- duty.c
+++ duty.c
@@ -10,7 +10,7 @@ void fillEvenNumbers(int *arr, int count) { // Function of filling the array wit
 int main() {             
     int count = 25 / 2; // Number of pairs between 1-25
     int *arr = (int *)malloc(count * sizeof(int)); //Memory allocation
-    
+    printf("Array size: %d\n", count); // Print the size of the array
     if (arr == NULL) {  // to preserve memory if malloc fails 
         printf("Memory allocation failed!\n");
         return 1;
